3a9049139f93e47f0018b1e137457764a0b96f0c
developer-junaid <qjunaid623@gmail.com>
2021-01-09 22:19:16 +0500

    Order data from firebase

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -46,7 +46,7 @@ const mapStateToProps = (state) => {
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: "projects" }, // Connect to projects collection
-    { collection: "notifications", limit: 3 }, // Connect to notifications collection
+    { collection: "projects", orderBy: ["createdAt", "desc"] }, // Connect to projects collection
+    { collection: "notifications", limit: 3, orderBy: ["time", "desc"] }, // Connect to notifications collection
   ])
 )(Dashboard);
